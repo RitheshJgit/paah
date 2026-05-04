537304d08918fc0e200fd4020e9e779e372c1943
RitheshJgit <ritheshjayapal@gmail.com>
2026-05-04 18:08:44 +0530

    Update firebaseAdmin.js

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,19 +1,19 @@
-import admin from "firebase-admin";
+// import admin from "firebase-admin";
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-    : undefined,
-};
+// const serviceAccount = {
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY
+//     ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+//     : undefined,
+// };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
 
-  console.log("🔥 Firebase Admin Initialized");
-}
+//   console.log("🔥 Firebase Admin Initialized");
+// }
 
-export default admin;
+// export default admin;
